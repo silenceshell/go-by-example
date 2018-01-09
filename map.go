ed655a9310e088511afc5b9b1612b6317ad12d51
silenceshell <hubottle@gmail.com>
2018-01-09 23:07:16 +0800

    add find

--- map.go
+++ map.go
@@ -16,6 +16,14 @@ func main() {
 		fmt.Println(v)
 	}
 
+	fmt.Println(xx["111"])
+	v, ok := xx["333"]
+	if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("333 not found")
+	}
+
 	zz := make([]string, 0, 3)
 	zz = append(zz, "v1/Secret")
 	zz = append(zz, "v1beta1/Deployment")
